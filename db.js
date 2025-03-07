872963e91012bb04ad6fa513a4af262a4d4c794c
Ruvindi Hulangamuwa <ruvindihulangamuwa@gmail.com>
2025-03-07 17:02:44 +0100

    made the env file

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "ruvi",
-  password: "ruvi",
-  host: "localhost", // ip address or domain name
-  port: 5432, // default Postgres port
-  database: "movies",
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
 });
 
 module.exports = {
